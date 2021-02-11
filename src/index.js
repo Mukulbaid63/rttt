88e03e2079f1a50577163d214bd97db82b1b794b
Mukulbaid63 <mukulbaid63@gmail.com>
2021-02-12 00:42:29 +0545

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+  
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import Game from "./components/Game";
+
+ReactDom.render(<Game />, document.getElementById("root"));
